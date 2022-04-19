101a59bc2f6356f1d183b7c1cc86218cadd80678
Ali Ismail <47851607+ali-ism@users.noreply.github.com>
2022-04-19 16:54:55 +0300

    Update AssemblyInfo.cs

--- FYP_PC/FYP_PC/Properties/AssemblyInfo.cs
+++ FYP_PC/FYP_PC/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FYP_PC")]
+[assembly: AssemblyTitle("ard_wid")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FYP_PC")]
+[assembly: AssemblyProduct("ard_wid")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
